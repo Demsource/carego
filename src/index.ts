4f9634fea7d09ae9d5c01f584f701d5ce0fe69ce
Demsource <demolomsadze@gmail.com>
2026-06-10 02:43:53 +0400

    Install and setup CORS

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import express from "express";
 import type { Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Enable CORS Middleware
+app.use(
+  cors({
+    origin: "*", // Allows access from any origin. For production, to be replaced with the frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.use(express.json());
 
